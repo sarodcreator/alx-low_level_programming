5c9035a52f4e894a29fd6f5f41b7e9e1d0082a23
Baridoo Saro <sarobaridoo@gmail.com>
2023-06-16 04:25:47 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,18 +3,19 @@
 /**
  * main - print the letters of the alphabet
  *
- * Description: print the letters of the alphabet in lower then in uppercase
+ * Description: print the letters of the alphabet except e, q
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char c = 'a';
+	char c; 
+	c = 'a';
 	while (c <= 'z'){
-		putchar(c);
-		c++;
-		putchar("\n");
+	    putchar(c);
+	    c++;
+	putchar('\n');
 	}
-	return 0;
+	return (0);
 }
