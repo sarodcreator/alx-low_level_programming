1439454e07bc50ecca3ed225c78d768a89214eaf
Baridoo Saro <sarobaridoo@gmail.com>
2023-06-20 05:46:38 -0700

    #include "main.h" /**  * main-"The program prints _putchar followed by a new line"  * File: "0-putchar.c"  * Return: "Always 0 (Success)"  */
    int main(void)
    {
    _putchar(_);
    _putchar(p);
    _putchar(u);
    _putchar(t);
    _putchar(c);
    _putchar(h);
    _putchar(a);
    _putchar(r);
    _putchar(n);
    
    return (0);
    }
    Footer
    © 2023 GitHub, Inc.
    Footer navigation
    Terms

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
 #include "main.h"
-
 /**
  * main-"The program prints _putchar followed by a new line"
  * File: "0-putchar.c"
  * Return: "Always 0 (Success)"
  */
 
-
 int main(void)
-{   
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
