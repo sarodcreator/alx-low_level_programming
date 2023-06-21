0cfb0483b2f56f720081353371581064324b4bd3
Baridoo Saro <sarobaridoo@gmail.com>
2023-06-21 14:41:36 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if(i == 0){
+		printf("%d is zero", i);
+	}
+	else if (i > 0){
+		printf("%d is positive", i);
+	}
+	else{
+		printf("%d is negative", i);
+	}
+}
