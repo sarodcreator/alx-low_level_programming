b7e3561e6623950c918e18b8c1e88c2d3a72677c
Baridoo Saro <sarobaridoo@gmail.com>
2023-06-30 06:38:50 -0700

    1-strncat.c;

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code for ALX School students.
+ * _strncat - concatenates n number of characters
+ * from src to dest
+ * @dest: destination string.
+ * @src: the source to be concatenated.
+ * @n: the number of bytes of src to be concatenated.
  *
- * Return: Always 0.
-*/
-
-int main(void)
+ * Return: dest.
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
+	int len;
 	char *ptr;
 
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strncat(s1, s2, 1);
-	printf("%s\n", s1);
-	printf("%s", s2);
-	printf("%s\n", ptr);
-	ptr = _strncat(s1, s2, 1024);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
-	return (0);
+	for (len = 0; dest[len] != '\0'; len++)
+		;
+	ptr = dest + len;
+	while (*src != '\0' && n--)
+		*ptr++ = *src++;
+
+	*ptr = '\0';
+	return (dest);
 }
