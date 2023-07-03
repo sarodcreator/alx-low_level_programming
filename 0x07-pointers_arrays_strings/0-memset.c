6a26ae783b47a4f509dcafb4434b6a5fe908366d
Baridoo Saro <sarobaridoo@gmail.com>
2023-07-03 16:43:01 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,21 +2,20 @@
 
 /**
  * _memset - function to set memory to array
- * @s - string
- * @b - constant byte
- * @n - number of bytes
+ * @s: string
+ * @b: constant byte
+ * @n: number of bytes
  *
  * Return: s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-
 	return (s);
 }
