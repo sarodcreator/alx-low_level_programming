e80e3ca1e16dc9ae681e03f10dba3061360beb3b
Baridoo Saro <sarobaridoo@gmail.com>
2023-07-05 15:03:35 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - print factorial of integer
+ * @n: integer to print factorial of
+ * Return: factorial of n
+ * -1 if n is negative
+ *  1 if n is 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return(1);
+	}
+	return (n * factorial(n - 1));
+}
