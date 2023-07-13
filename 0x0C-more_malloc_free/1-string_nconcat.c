8f9ad38fde17a50cc958190ca44cfd52ff6314a6
Baridoo Saro <sarobaridoo@gmail.com>
2023-07-13 13:39:46 -0700

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include "_strlen.c"
+/**
+ * string_nconcat - concatenate 2 strings
+ * @s1: first string
+ * @s2: second string
+ * @n: no. of bytes
+ *
+ * Return: char
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	int num, i, j, len;
+
+	num = n;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (num < 0)
+	{
+		return (NULL);
+	}
+	if (num >= _strlen(s2))
+	{
+		num = _strlen(s2);
+	}
+	len = _strlen(s1) + num + 1;
+	p = malloc(sizeof(*p) * len);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (j = 0; j < num; j++)
+	{
+		p[i + j] = s2[j];
+	}
+	p[i + j] = '\0';
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include "init.c"
+#include "checkfordigits.c"
+#include "mul.c"
+#include "print.c"
+
+/**
+ * main - multiply 2 numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: product error
+ */
+
+int main(int argc, char *argv[])
+{
+	int l1, l2, ln, ti, i;
+	char *a;
+	char *t;
+	char e[] = "Error\n";
+
+	if (argc != 3 || check_for_digits(argv))
+	{
+		for (ti = 0; e[ti]; ti++)
+			putchar(e[ti]);
+		exit(98);
+	}
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	ln = l1 + l2 + 1;
+	a = malloc(ln * sizeof(char));
+	if (a == NULL)
+	{
+		for (ti = 0; e[ti]; ti++)
+			putchar(e[ti]);
+		exit(98);
+	}
+	init(a, ln - 1);
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	{
+		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		if (t == NULL)
+		{
+			for (ti = 0; e[ti]; ti++)
+				putchar(e[ti]);
+			free(a);
+			exit(98);
+		}
+	}
+	_print(a, ln - 1);
+	return (0);
+}
