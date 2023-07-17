15810fbfaefe4194dcff6d262afc9d3eecb8e88d
Baridoo Saro <sarobaridoo@gmail.com>
2023-07-17 09:41:35 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * stdio.h - standard library
+ * a program that proints the name of a fileit was compiled from
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
