f9991a488705497f07b2ef10f31cf0a7c2273ebb
Baridoo Saro <sarobaridoo@gmail.com>
2023-07-17 10:06:46 -0700

    2-main.c

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * stdio.h - standard library
- * a program that proints the name of a fileit was compiled from
+ * main - prints the name of the file
+ * Return: 0
  */
-
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
-
 	return (0);
 }
