255da361c9201f059e2ce1a13ae20e35589c6d5e
Baridoo Saro <sarobaridoo@gmail.com>
2023-07-19 06:23:31 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a nameas is.
+ * @name: the name to be printed to stdout
+ * @f: a function pointer to the format of name
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if  (!f || !name)
+		return;
+	f(name);
+}
