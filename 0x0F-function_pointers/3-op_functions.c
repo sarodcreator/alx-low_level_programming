df229f9ba53686300aaf9fe9ef9f46ee1e47c1c4
Baridoo Saro <sarobaridoo@gmail.com>
2023-07-19 09:58:14 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - sum 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mul of 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div of 2 numbers
+ * @a: first numbe
+ * @b: second number 
+ *
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod of 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: div
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
