a94ee599d504fbcf32cff7c99703b24e0dabf79b
Baridoo Saro <sarobaridoo.com>
2023-08-24 14:42:35 -0700

    updated

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void pre_main_message(void) __attribue__((constructor));
+
+/**
+ * pre_main_message - Prints a message before main is executed.
+ */
+void pre_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
