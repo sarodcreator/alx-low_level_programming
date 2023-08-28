e849cf6df9489918a9dcde9066c4968214e8432b
Baridoo Saro <sarobaridoo.com>
2023-08-28 16:14:22 -0700

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,6 +14,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	new->n = n;
+	new->next =NULL;
 
 	if (*head == NULL)
 	{
@@ -28,8 +30,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			current->next = new;
 		}
 	}
-	new->n = n;
-	new->next = NULL;
 
 	return (new);
 }
